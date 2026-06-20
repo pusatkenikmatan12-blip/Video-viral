c8f6cc861ba4ab984a2b611a78a41656b2217af2
pusatkenikmatan12-blip <pusatkenikmatan12@gmail.com>
2026-06-20 23:58:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const searchInput = document.getElementById("searchInput");
+const videoCard = document.querySelector(".video-card");
 
+searchInput.addEventListener("keyup", () => {
+
+const keyword = searchInput.value.toLowerCase();
+const title = videoCard.dataset.title.toLowerCase();
+
+if(title.includes(keyword)){
+videoCard.style.display = "block";
+}else{
+videoCard.style.display = "none";
+}
+
+});
+
+function downloadVideo(){
+window.open(
+"https://cdn2.sliwtdrive.com/BIYDvcux1.mp4",
+"_blank"
+);
+}
+
+function playAgain(){
+const video = document.getElementById("mainVideo");
+video.currentTime = 0;
+video.play();
+}
